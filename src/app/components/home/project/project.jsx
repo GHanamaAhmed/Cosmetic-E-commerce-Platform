de26624e0a79facf1622ac23fe4db47d1c6b3308
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-11-17 21:55:21 +0000

    colors added

--- src/app/components/home/project/project.jsx
+++ src/app/components/home/project/project.jsx
@@ -16,35 +16,137 @@ export default function Project({textAlign,widthContainer}) {
         return () => globalThis.removeEventListener("resize", () => { })
     }, [])
     return (
-        <div className='w-full flex flex-col items-center gap-5'>
-            <TitleSection title={"Projects"} subTitle={"Things I’ve built so far"} textAlign={textAlign} />
-            <div className='w-full flex justify-around'>
-                {widthScreen >= 768 ? (
-                    <div className='w-full flex flex-col items-center gap-5'>
-                        <div className={`${widthContainer} flex gap-y-5 justify-between`}>
-                            <CardProject img={"./img/Rectangle 4.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                            <CardProject img={"./img/Rectangle 6.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                            <CardProject img={"./img/Rectangle 8.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                        </div>
-                        <div className={`${widthContainer} flex gap-y-5 justify-between`}>
-                            <CardProject img={"./img/Rectangle 12.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                            <CardProject img={"./img/Rectangle 13.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                            <CardProject img={"./img/Rectangle 14.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} />
-                        </div>
-                    </div>
-                ) : (<Swiper
-                    modules={[Navigation, A11y]}
-                    spaceBetween={10}
-                    slidesPerView={widthScreen < 640 ? 1 : 2}
-                >
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 4.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 6.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 8.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 12.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 13.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                    <SwiperSlide className='content-center flex justify-center'><CardProject img={"./img/Rectangle 14.png"} title={"Project Tile goes here"} description={"This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content"} tech={"HTML , JavaScript, SASS, React"} url={""} urlGithub={""} /></SwiperSlide>
-                </Swiper>)}
-            </div>
-        </div>
-    )
+			<div className="w-full h-full flex flex-col items-center gap-5">
+				<TitleSection
+					title={"Projects"}
+					subTitle={"Things I’ve built so far"}
+					textAlign={textAlign}
+				/>
+				<div className="w-full h-full flex justify-around">
+					{widthScreen >= 768 ? (
+						<div className="w-full h-full flex flex-col items-center gap-5">
+							<div className={`${widthContainer} h-fit flex gap-y-5 justify-between`}>
+								<CardProject
+									img={"/img/Rectangle 4.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+								<CardProject
+									img={"/img/Rectangle 6.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+								<CardProject
+									img={"/img/Rectangle 8.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</div>
+							<div className={`${widthContainer} flex gap-y-5 justify-between`}>
+								<CardProject
+									img={"/img/Rectangle 12.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+								<CardProject
+									img={"/img/Rectangle 13.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+								<CardProject
+									img={"/img/Rectangle 14.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</div>
+						</div>
+					) : (
+						<Swiper
+							modules={[Navigation, A11y]}
+							spaceBetween={10}
+							slidesPerView={widthScreen < 640 ? 1 : 2}
+						>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 4.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 6.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 8.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 12.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 13.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+							<SwiperSlide className="content-center flex justify-center">
+								<CardProject
+									img={"/img/Rectangle 14.png"}
+									title={"المنتج"}
+									description={"lorem ipsum sdssdfka;sdlfkjldskjf"}
+									colors={["bg-green-500", "bg-blue-500", "bg-red-500", "bg-yellow-500"]}
+									url={""}
+									urlGithub={""}
+								/>
+							</SwiperSlide>
+						</Swiper>
+					)}
+				</div>
+			</div>
+		);
 }
