5d1fcf8beb412a68b69d78b92c73984594319b79
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-11-20 18:52:04 +0000

    wow

--- src/app/components/payment.jsx
+++ src/app/components/payment.jsx
@@ -4,7 +4,7 @@ import ProductINfo from './ProductINfo'
 
 export default function Payment() {
   return (
-		<div className="w-10/12 h-full flex flex-row justify-between items-center gap-16 mb-10">
+		<div className="w-10/12 h-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-16 mb-10">
 			<ProductINfo />
 			<ClientINfo />
 		</div>
