456cd3939eea72fd3131d71c8e562abc1e6ee672
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-11-22 19:00:13 +0000

    hydration error fixed

--- src/app/components/product/product.jsx
+++ src/app/components/product/product.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide} from "swiper/react";
 import { A11y, Grid, Navigation, Pagination } from "swiper/modules";
 import { FaAngleDown } from "react-icons/fa6";
 import "swiper/css";
@@ -44,6 +44,10 @@ export default function Product() {
   const [selectedSize, setselectedSize] = useState();
   const [selectedColor, setselectedColor] = useState();
   const [isShowDescription, setIsShowDescription] = useState(false);
+  const [hydrated, setHydrated] = useState(false);
+	useEffect(() => {
+		setHydrated(true);
+	}, []);
   const { width } = useWidth();
   const numOfSlide = () => {
     if (width > 767) {
@@ -71,154 +75,154 @@ export default function Product() {
     }
   };
   return (
-    <div className="pt-[75px] ">
-      <div className="hidden relative h-[150px] md:flex px-10 items-end py-6">
-        <Image className="-z-10" src="/img/image 3.jpg" fill />
-        <p className="text-white text-4xl">خطوات الشراء</p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:pt-7">
-        <div className="relative w-full h-[229px] md:min-h-[400px] px-3">
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            modules={[Pagination]}
-            className="w-full h-full"
-          >
-            {[...Array(numOfSlide())].map((size, index) => (
-              <SwiperSlide
-                className={`swiper-grid relative h-full w-full`}
-                key={index}
-              >
-                {[...Array(numImage)]
-                  .slice(imageStart(index), imageEnd(index))
-                  .map((_, ind) => (
-                    <div key={ind} className="relative w-full h-full">
-                      <Image
-                        className="object-contain"
-                        src="/img/feature-product-1.jpg"
-                        fill
-                        alt=""
-                      />
-                    </div>
-                  ))}
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        <div className="px-5 flex flex-col gap-3 w-full">
-          <p className="text-solidHeading text-2xl">المنتج 1</p>
-          <p className="text-mainColor">200 دج</p>
-          <div className="grid w-full">
-            <Swiper
-              slidesPerView={"auto"}
-              spaceBetween={10}
-              modules={[Pagination]}
-              className="mySwiper w-full"
-            >
-              {sizes.map((size, index) => (
-                <SwiperSlide className="swiper-product" key={index}>
-                  <button
-                    onClick={() => setselectedSize(index)}
-                    className={`${
-                      index == selectedSize
-                        ? "border-none bg-mainColor text-white"
-                        : "border-mainColor border-[1.5px]"
-                    }  font-medium  active:border-none active:bg-mainColor active:text-white p-2 rounded-sm`}
-                  >
-                    {size}
-                  </button>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-          <Swiper
-            slidesPerView={"auto"}
-            spaceBetween={10}
-            autoHeight={true}
-            className="w-full "
-          >
-            {colors.map((color, index) => (
-              <SwiperSlide key={index} className="swiper-card ">
-                <button
-                  onClick={() => setselectedColor(index)}
-                  key={index}
-                  className={`rounded-full ${
-                    selectedColor == index ? "ring-4" : ""
-                  } z-50 w-7 h-7 ${color}`}
-                ></button>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <div className="w-full grid grid-cols-2 gap-2">
-            <button
-              className={` font-medium border-mainColor border-[1.5px] active:border-none active:bg-mainColor active:text-white p-2 rounded-sm`}
-            >
-              طلب
-            </button>
-            <button
-              className={`font-medium border-mainColor border-[1.5px] active:border-none active:bg-[#989898] active:text-white p-2 rounded-sm`}
-            >
-              اضافة للسلة
-            </button>
-          </div>
-          <div className="ont-medium relative w-full border-mainColor border-[1.5px] p-2 rounded-sm">
-            <button
-              className={`w-full`}
-              onClick={() => setIsShowDescription(!isShowDescription)}
-            >
-              معلومات المنتج
-            </button>
-            <FaAngleDown
-              className={`${
-                isShowDescription ? "rotate-180" : ""
-              } absolute left-3 top-1/2 -translate-y-1/2`}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="px-3 py-5 flex flex-col gap-3">
-        <p className="text-2xl text-solidHeading">مقترحات</p>
-        <div className="flex justify-center w-full">
-          <Swiper
-            slidesPerView={"auto"}
-            navigation={true}
-            modules={[Navigation]}
-            className="mySwiper w-full"
-          >
-            <SwiperSlide className="swiper-card">
-              <CardProject
-                img={"/img/Rectangle 6.png"}
-                title={"المنتج"}
-                colors={[
-                  "bg-green-500",
-                  "bg-blue-500",
-                  "bg-red-500",
-                  "bg-yellow-500",
-                ]}
-                sizes={["100 مل", "100 مل", "100 مل", "100 مل"]}
-                url={""}
-                urlGithub={""}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <CardProject
-                img={"/img/Rectangle 6.png"}
-                title={"المنتج"}
-                colors={[
-                  "bg-green-500",
-                  "bg-blue-500",
-                  "bg-red-500",
-                  "bg-yellow-500",
-                ]}
-                sizes={["100 مل", "100 مل", "100 مل", "100 مل"]}
-                url={""}
-                urlGithub={""}
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
-      <Contacts />
-    </div>
-  );
+		<div className="pt-[75px] ">
+			<div className="hidden relative h-[150px] md:flex px-10 items-end py-6">
+				<Image className="-z-10" src="/img/image 3.jpg" fill alt="" />
+				<p className="text-white text-4xl">خطوات الشراء</p>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 md:pt-7">
+				<div className="relative w-full h-[230px] md:min-h-[400px] px-3">
+					<Swiper
+						slidesPerView={1}
+						spaceBetween={30}
+						modules={[Pagination]}
+						className="w-full h-full   shadow-[inset_0px_20px_20px_10px_#f7fafc]"
+					>
+						{[...Array(hydrated ? numOfSlide():0)].map((size, index) => (
+							<SwiperSlide
+								className={`swiper-grid relative h-full w-full m-1 `}
+								key={index}
+							>
+								{[...Array(numImage)]
+									.slice(imageStart(index), imageEnd(index))
+									.map((_, ind) => (
+										<div key={ind} className="relative w-full h-full ">
+											<Image
+												className="object-contain my-2 ml-2"
+												src="/img/feature-product-1.jpg"
+												fill
+												alt="img"
+											/>
+										</div>
+									))}
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
+				<div className="px-5 flex flex-col gap-3 w-full">
+					<p className="text-solidHeading text-2xl">المنتج 1</p>
+					<p className="text-mainColor">200 دج</p>
+					<div className="grid w-full">
+						<Swiper
+							slidesPerView={"auto"}
+							spaceBetween={10}
+							modules={[Pagination]}
+							className="mySwiper w-full"
+						>
+							{sizes.map((size, index) => (
+								<SwiperSlide className="swiper-product" key={index}>
+									<button
+										onClick={() => setselectedSize(index)}
+										className={`${
+											index == selectedSize
+												? "border-none bg-mainColor text-white"
+												: "border-mainColor border-[1.5px]"
+										}  font-medium  active:border-none active:bg-mainColor active:text-white p-2 rounded-sm`}
+									>
+										{size}
+									</button>
+								</SwiperSlide>
+							))}
+						</Swiper>
+					</div>
+					<Swiper
+						slidesPerView={"auto"}
+						spaceBetween={10}
+						autoHeight={true}
+						className="w-full "
+					>
+						{colors.map((color, index) => (
+							<SwiperSlide key={index} className="swiper-card ">
+								<button
+									onClick={() => setselectedColor(index)}
+									key={index}
+									className={`rounded-full ${
+										selectedColor == index ? "ring-4" : ""
+									} z-50 w-7 h-7 ${color}`}
+								></button>
+							</SwiperSlide>
+						))}
+					</Swiper>
+					<div className="w-full grid grid-cols-2 gap-2">
+						<button
+							className={` font-medium border-mainColor border-[1.5px] active:border-none active:bg-mainColor active:text-white p-2 rounded-sm`}
+						>
+							طلب
+						</button>
+						<button
+							className={`font-medium border-mainColor border-[1.5px] active:border-none active:bg-[#989898] active:text-white p-2 rounded-sm`}
+						>
+							اضافة للسلة
+						</button>
+					</div>
+					<div className="ont-medium relative w-full border-mainColor border-[1.5px] p-2 rounded-sm">
+						<button
+							className={`w-full`}
+							onClick={() => setIsShowDescription(!isShowDescription)}
+						>
+							معلومات المنتج
+						</button>
+						<FaAngleDown
+							className={`${
+								isShowDescription ? "rotate-180" : ""
+							} absolute left-3 top-1/2 -translate-y-1/2`}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="px-3 py-5 flex flex-col gap-3">
+				<p className="text-2xl text-solidHeading">مقترحات</p>
+				<div className="flex justify-center w-full">
+					<Swiper
+						slidesPerView={"auto"}
+						navigation={true}
+						modules={[Navigation]}
+						className="mySwiper w-full"
+					>
+						<SwiperSlide className="swiper-card">
+							<CardProject
+								img={"/img/Rectangle 6.png"}
+								title={"المنتج"}
+								colors={[
+									"bg-green-500",
+									"bg-blue-500",
+									"bg-red-500",
+									"bg-yellow-500",
+								]}
+								sizes={["100 مل", "100 مل", "100 مل", "100 مل"]}
+								url={""}
+								urlGithub={""}
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<CardProject
+								img={"/img/Rectangle 6.png"}
+								title={"المنتج"}
+								colors={[
+									"bg-green-500",
+									"bg-blue-500",
+									"bg-red-500",
+									"bg-yellow-500",
+								]}
+								sizes={["100 مل", "100 مل", "100 مل", "100 مل"]}
+								url={""}
+								urlGithub={""}
+							/>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+			<Contacts />
+		</div>
+	);
 }
