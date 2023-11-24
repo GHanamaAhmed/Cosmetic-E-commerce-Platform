dafcc57c118e313dfb7efef223ca7216799a5bbe
GHanamaAhmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-11-24 17:13:50 -0500

    update

--- src/app/components/ProductINfo.jsx
+++ src/app/components/ProductINfo.jsx
@@ -5,30 +5,30 @@ export default function ProductINfo() {
   return (
     <div className=" w-full row-start-2 md:row-start-1">
       <div className="w-full flex flex-col justify-center h-fit  p-2 bg-SolidHeadingDarkMode">
-        <p className="text-2xl ">في سلتك</p>
+        <p className="text-xl ">في سلتك</p>
       </div>
       <div className="w-full flex flex-row justify-between items-center gap-5">
         <div className="w-11/12">
           <div className="w-full flex justify-between">
-            <p className="text-lg ">المجموع الفرعي</p>
-            <p className="text-lg ">3300دج</p>
+            <p className="font-thin">المجموع الفرعي</p>
+            <p className="font-mono text-sm ">3300دج</p>
           </div>
           <div className="w-full flex justify-between">
-            <p className="text-lg "> التوصيل</p>
-            <p className="text-lg ">3300دج</p>
+            <p className="font-mono text-sm "> التوصيل</p>
+            <p className="font-mono text-sm ">3300دج</p>
           </div>
           <div className="w-full flex justify-between">
-            <p className="text-lg "> التخفيض</p>
-            <p className="text-lg ">3300دج</p>
+            <p className="font-mono text-sm "> التخفيض</p>
+            <p className="font-mono text-sm ">3300دج</p>
           </div>
           <div className="w-full flex justify-between">
-            <p className="text-lg  font-semibold">المجموع</p>
-            <p className="text-lg text-mainColor">3300دج</p>
+            <p className="font-semibold">المجموع</p>
+            <p className="font-mono text-sm text-mainColor">3300دج</p>
           </div>
         </div>
       </div>
-      <div className="w-full flex flex-col my-3 gap-3 bg-lightContent px-2 py-3">
-        <p className="text-lg ">مكان التوصيل</p>
+      <div className="w-full flex flex-col my-3 gap-3 bg-SolidHeadingDarkMode px-2 py-3">
+        <p className="text-xl ">مكان التوصيل</p>
         <div className="flex flex-row w-full gap-5">
           {" "}
           <input
@@ -38,7 +38,7 @@ export default function ProductINfo() {
             value={"choice"}
             className="accent-black accent scale-125"
           />
-          <label htmlFor="agence">التوصيل للمكتب</label>
+          <label htmlFor="agence" className="text-sm font-mono">التوصيل للمكتب</label>
         </div>
         <div className="flex flex-row w-full gap-5">
           <input
@@ -48,34 +48,23 @@ export default function ProductINfo() {
             value={"choice"}
             className="accent-black accent scale-125"
           />
-          <label htmlFor="home"> التوصيل للمنزل &#8205; &#8205;</label>
+          <label htmlFor="home" className="text-sm font-mono"> التوصيل للمنزل &#8205; &#8205;</label>
         </div>
       </div>
-      <div className=" w-1/2 h-1/2 flex flex-col gap-5 ">
+      <div className="flex flex-col gap-5">
         <div className="flex flex-row gap-5 ">
           <Image
             src={"/img/feature-product-1.jpg"}
             width={200}
             height={200}
-            className="rounded-lg"
+            className="rounded-lg w-28 h-28"
           />
-          <div>
-            <p className="text-lg ">3300دج</p>
-            <p className="text-lg ">3300دج</p>
-            <p className="text-lg ">3300دج</p>
-          </div>
-        </div>
-        <div className="flex flex-row gap-5 ">
-          <Image
-            src={"/img/feature-product-1.jpg"}
-            width={200}
-            height={200}
-            className="rounded-lg"
-          />
-          <div>
-            <p className="text-lg ">3300دج</p>
-            <p className="text-lg ">3300دج</p>
-            <p className="text-lg ">3300دج</p>
+          <div className="flex flex-col justify-between">
+            <p className="font-mono text-sm ">الاسم</p>
+            <p className="font-mono text-sm">اللون:</p>
+            <p className="font-mono text-sm ">الحجم :متوسط</p>
+            <p className="font-mono text-sm ">الكمية: 10</p>
+            <p className="font-mono text-sm ">السعر: 200دج</p>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			FontFace: {
-				"Hacen-Tunisia": "Hacen Tunisia",
-			},
-			colors: {
-				transparent: "transparent",
-				current: "currentColor",
-				solidHeading: "#42446E",
-				darkContent: "#2f3e46",
-				SolidHeadingDarkMode: "#cccccc",
-				lightContent: "#A7A7A7",
-				buttonText: "#018C0F",
-				buttonSuccess: "#D7FFE0",
-				darkMode: "#191919",
-				textLight: "#d9d9d9",
-				cardDark: "#363636",
-				darkModeSecondary: "#141414",
-				caderSection: "#F4F6F5",
-				mainColor: "#FF4A4A",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      FontFace: {
+        "Hacen-Tunisia": "Hacen Tunisia",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        solidHeading: "#42446E",
+        darkContent: "#2f3e46",
+        SolidHeadingDarkMode: "#cccccc",
+        lightContent: "#A7A7A7",
+        buttonText: "#018C0F",
+        buttonSuccess: "#D7FFE0",
+        darkMode: "#191919",
+        textLight: "#d9d9d9",
+        cardDark: "#363636",
+        darkModeSecondary: "#141414",
+        caderSection: "#F4F6F5",
+        mainColor: "#FF4A4A",
+      },
+    },
+  },
+  plugins: [],
 };
